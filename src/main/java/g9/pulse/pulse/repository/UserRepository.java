c5b387fbc325dd3a62e783c5cff0c12bee149958
Medhavee Amarna <amarnah-cs22062@stu.kln.ac.lk>
2026-06-12 00:10:33 +0530

    Create UserRepository.java

--- src/main/java/g9/pulse/pulse/repository/UserRepository.java
+++ src/main/java/g9/pulse/pulse/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package g9.pulse.pulse.repository;
 
-public class UserRepository {
+import g9.pulse.pulse.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+         Optional<User> findByEmail(String email);
+
+         boolean existsByEmail(String email);
+
+
 }
