26342b715a15e6e74f08d7e99945ddf9f568cdaa
Medhavee Amarna <amarnah-cs22062@stu.kln.ac.lk>
2026-06-12 23:57:12 +0530

    Create SecurityConfig.java

---
+++ src/main/java/g9/pulse/pulse/security/SecurityConfig.java
@@ -0,0 +1,74 @@
+package g9.pulse.pulse.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+   public SecurityFilterChain filterChain(HttpSecurity http)
+        throws Exception{
+
+        return http
+
+        .csrf(csrf -> csrf.disable())
+
+
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers(
+                                "/",
+                                "/login",
+                                "/register",
+                                "/css/**"
+                        ).permitAll()
+
+
+                        .anyRequest()
+                        .authenticated()
+
+                )
+
+
+                .formLogin(form -> form
+
+                        .loginPage("/login")
+
+                        .defaultSuccessUrl("/home",true)
+
+                        .permitAll()
+
+                )
+
+
+                .logout(logout -> logout
+
+                        .logoutSuccessUrl("/login")
+
+                        .invalidateHttpSession(true)
+
+                )
+
+
+                .build();
+
+    }
+
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+
+        return new BCryptPasswordEncoder();
+
+
+
+    }
+}
