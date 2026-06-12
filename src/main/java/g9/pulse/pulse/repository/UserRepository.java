dc9f17eb5779011a056b7a724b5c942f3969ded5
Manoli Lakshani Weerakkodi <weerakk-cs22058@stu.kln.ac.lk>
2026-06-12 23:49:46 +0530

    Add SearchController and Update UserRepository

--- src/main/java/g9/pulse/pulse/repository/UserRepository.java
+++ src/main/java/g9/pulse/pulse/repository/UserRepository.java
@@ -3,12 +3,14 @@ package g9.pulse.pulse.repository;
 import g9.pulse.pulse.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
          Optional<User> findByEmail(String email);
 
          boolean existsByEmail(String email);
+    List<User> findByFirstNameContainingIgnoreCase(String firstName);
 
 
 }
