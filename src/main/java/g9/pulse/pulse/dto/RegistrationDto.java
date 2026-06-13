ccb2fca15ddb266df0484d233353738a1b444cfe
Medhavee Amarna <amarnah-cs22062@stu.kln.ac.lk>
2026-06-13 10:52:40 +0530

    Update RegistrationDto.java

--- src/main/java/g9/pulse/pulse/dto/RegistrationDto.java
+++ src/main/java/g9/pulse/pulse/dto/RegistrationDto.java
@@ -18,6 +18,9 @@ import jakarta.validation.constraints.NotBlank;
         @NotBlank
         private String password;
 
+        @NotBlank
+        private String confirmPassword;
+
 
         public String getFirstName() {
             return firstName;
@@ -38,20 +41,31 @@ import jakarta.validation.constraints.NotBlank;
 
 
         public String getEmail() {
+
             return email;
         }
 
         public void setEmail(String email) {
+
             this.email = email;
         }
 
 
         public String getPassword() {
+
             return password;
         }
 
         public void setPassword(String password) {
             this.password = password;
+        }
+
+        public String getConfirmPassword() {
+            return confirmPassword;
+        }
+
+        public void setConfirmPassword(String confirmPassword) {
+            this.confirmPassword = confirmPassword;
         }
     }
 
