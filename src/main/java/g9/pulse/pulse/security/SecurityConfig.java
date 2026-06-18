0b8eb5ae409c78bfc04592791bd8aeff3f474064
Medhavee Amarna <amarnah-cs22062@stu.kln.ac.lk>
2026-06-18 12:54:54 +0530

    Update login.html and create style.css

--- src/main/java/g9/pulse/pulse/security/SecurityConfig.java
+++ src/main/java/g9/pulse/pulse/security/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
                                 "/",
                                 "/login",
                                 "/register",
-                                "/css/**"
+                                "/style.css/**"
                         ).permitAll()
 
 
