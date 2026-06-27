c1a0ddf98b44a1ee6a22946118f9a9aa8d33ee77
DilshaKalpani <sheharadilsha035@gmail.com>
2026-06-27 17:02:48 +0530

    desinged friends and request UI

---
+++ src/main/resources/static/js/friends.js
@@ -0,0 +1,55 @@
+function initializeFriendButtons() {
+
+    document.querySelectorAll(".send-request").forEach(btn => {
+
+        btn.addEventListener("click", function () {
+
+            const receiverId = this.dataset.id;
+
+            const senderId = 1; // logged user
+
+            fetch(`/friends/request/${receiverId}?senderId=${senderId}`, {
+                method: "POST"
+            })
+            .then(res => res.text())
+            .then(msg => alert(msg));
+        });
+    });
+}
+
+function initializeAcceptRejectButtons() {
+
+    document.querySelectorAll(".accept").forEach(btn => {
+
+        btn.addEventListener("click", function () {
+
+            const id = this.dataset.id;
+
+            fetch(`/friends/accept/${id}`, {
+                method: "POST"
+            })
+            .then(res => res.text())
+            .then(msg => {
+                alert(msg);
+                location.reload();
+            });
+        });
+    });
+
+    document.querySelectorAll(".reject").forEach(btn => {
+
+        btn.addEventListener("click", function () {
+
+            const id = this.dataset.id;
+
+            fetch(`/friends/reject/${id}`, {
+                method: "POST"
+            })
+            .then(res => res.text())
+            .then(msg => {
+                alert(msg);
+                location.reload();
+            });
+        });
+    });
+}
