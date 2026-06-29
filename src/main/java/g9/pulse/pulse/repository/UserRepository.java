c17ae30d9d55b49af057491720e8ef8c38e0a12f
Manoli Lakshani Weerakkodi <weerakk-cs22058@stu.kln.ac.lk>
2026-06-29 22:57:18 +0530

    update user repository and service

--- src/main/java/g9/pulse/pulse/repository/UserRepository.java
+++ src/main/java/g9/pulse/pulse/repository/UserRepository.java
@@ -2,6 +2,9 @@ package g9.pulse.pulse.repository;
 
 import g9.pulse.pulse.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.Optional;
 import java.util.List;
 
@@ -13,6 +16,8 @@ public interface UserRepository extends JpaRepository<User,Long> {
 
     List<User> findByFirstNameContainingIgnoreCase(String firstName);
     List<User> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(String firstName, String lastName);
+    @Query("SELECT u FROM User u WHERE LOWER(CONCAT(u.firstName, ' ', u.lastName)) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<User> searchByFullNameContaining(@Param("keyword") String keyword);
 
 
 }
